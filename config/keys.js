91986f7422c883fd65393d0edc56d3d4f0d90932
Det-tech <proninkirilll228@gmail.com>
2022-08-10 14:54:48 -0700

    Updated: mongodb uri

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mongoURI: "mongodb://test:4L9FWr1buHIYaAst@cluster0-shard-00-00.qwixr.mongodb.net:27017,cluster0-shard-00-01.qwixr.mongodb.net:27017,cluster0-shard-00-02.qwixr.mongodb.net:27017/flea?ssl=true&replicaSet=atlas-wu5x60-shard-0&authSource=admin&retryWrites=true&w=majority",
-  // mongoURI: "mongodb://localhost:27017/fleamint",
+  // mongoURI: "mongodb://test:4L9FWr1buHIYaAst@cluster0-shard-00-00.qwixr.mongodb.net:27017,cluster0-shard-00-01.qwixr.mongodb.net:27017,cluster0-shard-00-02.qwixr.mongodb.net:27017/flea?ssl=true&replicaSet=atlas-wu5x60-shard-0&authSource=admin&retryWrites=true&w=majority",
+  mongoURI: "mongodb://test:BihV70zLC2LxoKOO@ac-5ryrjss-shard-00-00.ifzrz5n.mongodb.net:27017,ac-5ryrjss-shard-00-01.ifzrz5n.mongodb.net:27017,ac-5ryrjss-shard-00-02.ifzrz5n.mongodb.net:27017/?ssl=true&replicaSet=atlas-p37nes-shard-0&authSource=admin&retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
 //mongodb://test:4L9FWr1buHIYaAst@cluster0-shard-00-00.qwixr.mongodb.net:27017,cluster0-shard-00-01.qwixr.mongodb.net:27017,cluster0-shard-00-02.qwixr.mongodb.net:27017/flea?ssl=true&replicaSet=atlas-wu5x60-shard-0&authSource=admin&retryWrites=true&w=majority
